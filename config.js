33a7beacab294d306e87187b9f374847dbed5716
wanghyun <102793640+wanghyun@users.noreply.github.com>
2026-03-15 12:38:05 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+/**
+ * 역할:
+ * 프로젝트 전체 공통 설정 파일
+ * Apps Script URL, 관리자 키, 사이트명 등을 한 곳에서 관리한다.
+ */
+window.APP_CONFIG = {
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwADE5wJHtNwKB-Y73cJOl_1JrM7WEdGOki9_Xg9XqETUQAv9GXmJTwaFITawhoTu1S/exec',
+  SITE_NAME: '외주 견적 SaaS',
+  ADMIN_KEY_STORAGE_NAME: 'HB_ADMIN_KEY'
+};
