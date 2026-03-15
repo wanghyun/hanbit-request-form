803a93c93dfcf62acd0be39ed1e331b0cb105a2a
wanghyun <102793640+wanghyun@users.noreply.github.com>
2026-03-15 14:18:32 +0900

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
  * Apps Script URL, 관리자 키, 사이트명, 연락처, 에드민 키 등을 한 곳에서 관리한다.
  */
 window.APP_CONFIG = {
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwADE5wJHtNwKB-Y73cJOl_1JrM7WEdGOki9_Xg9XqETUQAv9GXmJTwaFITawhoTu1S/exec',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxuadp2XlBlNuvoLFSug5xjL1hdkL8_iaousjLUXIoHad_8ZWQVjg7WKbIMyIV7kQjJ/exec',
   SITE_NAME: '웹사이트 외주 견적 신청서',
   ADMIN_KEY_STORAGE_NAME: 'HB_ADMIN_KEY',
   COMPANY_PHONE: '010-5008-8985',
